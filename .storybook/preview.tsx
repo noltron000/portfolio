6263040707d7660ce1251975716a7bd69e875c0d
Nolan <noltron000@gmail.com>
2021-09-12 21:20:25 -0400

    Updating storybook configuration

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router';
 import { addDecorator } from '@storybook/react';
-import './index.css';
+import '../src/index.css'
 
 addDecorator((story) => (
 	<MemoryRouter initialEntries={['/']}>
 		{story()}
 	</MemoryRouter>
 ));
+
+const parameters = {
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+}
+
+export {parameters}
