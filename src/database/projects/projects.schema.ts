a757876c5a73061585cf13f4fcc453b38e499a9c
Nolan <noltron000@gmail.com>
2021-09-13 13:33:09 -0400

    Updating schema to have methods

--- src/database/projects/projects.schema.ts
+++ src/database/projects/projects.schema.ts
@@ -1,5 +1,8 @@
 import { Schema } from "mongoose";
+import * as Statics from "./projects.statics"
+import * as Methods from "./projects.methods"
 
+// Design the schema in accordance with its types.
 const ProjectSchema = new Schema({
   name: String,
   description: String,
@@ -9,5 +12,15 @@ const ProjectSchema = new Schema({
     required: false,
   },
 });
+
+// Assign all static methods exported for a project.
+Object.entries(Statics).forEach(([key, method]) => {
+  ProjectSchema.statics[key] = method
+})
+
+// Assign all instance methods exported for a project.
+Object.entries(Methods).forEach(([key, method]) => {
+  ProjectSchema.methods[key] = method
+})
 
 export default ProjectSchema;
