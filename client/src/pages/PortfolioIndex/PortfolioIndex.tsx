3bb22130aa49322cb66098c4ebb59c26afa28d54
Nolan <noltron000@gmail.com>
2021-09-21 11:24:36 -0400

    Obtaining projects from backend

--- client/src/pages/PortfolioIndex/PortfolioIndex.tsx
+++ client/src/pages/PortfolioIndex/PortfolioIndex.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PortfolioIndex = () => {
-	const items = [];
+	const [projects, setProjects] = useState<null | Record<string, unknown>>(null);
+
+	const fetchProjects = async () => {
+		try {
+			const response = await fetch('/projects', {
+				method: 'GET',
+			});
+			const dbProjects = await response.json();
+			setProjects(dbProjects);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	useEffect(() => {
+		fetchProjects();
+	}, []);
 
 	// Load pre-paginated amount of portfolio items.
 	// Return a list of each portfolio item, linking to each.
@@ -9,6 +25,7 @@ const PortfolioIndex = () => {
 		<>
 			<h2>Portfolio Index</h2>
 			<p>Here&apos;s a list of items.</p>
+			<pre><code>{JSON.stringify(projects, undefined, '\t')}</code></pre>
 		</>
 	);
 };
