e868332030bef283d17c9241dedacd64bd8ce2f1
Nolan <noltron000@gmail.com>
2021-09-21 13:49:43 -0400

    Creating show project component

--- client/src/pages/ProjectShow/ProjectShow.tsx
+++ client/src/pages/ProjectShow/ProjectShow.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
-const ProjectShow = () => {
-	const item = null;
+const ProjectShow = (): JSX.Element => {
+	// Grab the url parameters.
+	const { id } = useParams<{id: string}>();
+
+	// Gather the projects asyncronously using state, fetch, and effects.
+	const [project, setProject] = useState<any>({});
+	const fetchProject = async () => {
+		try {
+			const response = await fetch(`/projects/${id}`, { method: 'GET' });
+			const dbProject = await response.json();
+			setProject(dbProject);
+		}
+		catch (err) {
+			console.error(err);
+		}
+	};
+	useEffect(() => {
+		fetchProject();
+	}, []);
 
 	// Load data on the portfolio item and display it nicely.
 	return (
 		<>
 			<h2>Show Portfolio Item</h2>
 			<p>Here&apos;s the chosen item!</p>
+			<div>
+				<h3>{project.name}</h3>
+				<p>{project.description}</p>
+			</div>
 		</>
 	);
 };
