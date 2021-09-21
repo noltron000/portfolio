b04d3b2e31fdfda0455877bb658b68e9f955163a
Nolan <noltron000@gmail.com>
2021-09-21 14:19:11 -0400

    Using react-router-dom over react-router

--- client/src/pages/ProjectShow/ProjectShow.tsx
+++ client/src/pages/ProjectShow/ProjectShow.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 
 const ProjectShow = (): JSX.Element => {
 	// Grab the url parameters.
