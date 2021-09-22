0d475fd5b278dc267f42323e2d1ae6b0ab73b9c7
Nolan <noltron000@gmail.com>
2021-09-22 14:49:32 -0400

    Linting

--- client/src/pages/App/App.stories.tsx
+++ client/src/pages/App/App.stories.tsx
@@ -7,6 +7,6 @@ export default {
 	component: App,
 } as ComponentMeta<typeof App>;
 
-const Template: ComponentStory<typeof App> = (args) => <App />;
+const Template: ComponentStory<typeof App> = () => <App />;
 
 export const Default = Template.bind({});
--- client/src/pages/App/App.tsx
+++ client/src/pages/App/App.tsx
@@ -3,44 +3,42 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/react-logo.svg';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit
-					{' '}
-					<code>src/App.tsx</code>
-					{' '}
-					and save to reload.
+const App = (): JSX.Element => (
+	<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<p>
+				Edit
+				{' '}
+				<code>src/App.tsx</code>
+				{' '}
+				and save to reload.
 
-					<br />
+				<br />
 
-					Navigate to
-					{' '}
-					<Link
-						className="App-link"
-						to="/"
-					>
-						Home
-					</Link>
-					{' '}
-					to test another page.
-				</p>
-				<p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</p>
-			</header>
-		</div>
-	);
-}
+				Navigate to
+				{' '}
+				<Link
+					className="App-link"
+					to="/"
+				>
+					Home
+				</Link>
+				{' '}
+				to test another page.
+			</p>
+			<p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</p>
+		</header>
+	</div>
+);
 
 export default App;
